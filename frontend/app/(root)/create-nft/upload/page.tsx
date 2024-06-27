9abd4ff138e941d5340c0d60e9abe686cf708aa9
kalanishivam <kalanishivam@gmail.com>
2024-06-27 18:34:44 +0530

    form -partial

--- frontend/app/(root)/create-nft/upload/page.tsx
+++ frontend/app/(root)/create-nft/upload/page.tsx
@@ -8,10 +8,10 @@ import HeroNft from '@/components/HeroNft'
 import InputText from '@/components/InputText'
 import Image from 'next/image'
 import React, { useState } from 'react'
-import {useDropzone} from 'react-dropzone';
+import { useDropzone } from 'react-dropzone';
 import * as z from "zod";
 import { useForm } from "react-hook-form";
-import {zodResolver} from '@hookform/resolvers/zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 const formSchema = z.object({
   email: z.string().email(),
@@ -24,46 +24,48 @@ const formSchema = z.object({
     }, {
       message: 'Invalid file type. Allowed types: .jpg, .jpeg, .png, .svg, .mp4, .gif'
     }),
-  name: z.string().nonempty({ message: 'Name Of Your NFT is required' }),
-  price: z.string().nonempty({ message: 'Price is required' }),
-  collection: z.string().nonempty({ message: 'Collection is required' }),
-  royalties: z.string().nonempty({ message: 'Royalties are required' }),
+  name: z.string().min(1, { message: 'Name Of Your NFT is required' }),
+  price: z.string().min(1, { message: 'Price is required' }),
+  collection: z.string().min(1, { message: 'Collection is required' }),
+  royalties: z.string().min(1, { message: 'Royalties are required' }),
   description: z.string().optional()
 });
 
 const UploadNFt = () => {
-  const formOfUpload = useForm<z.infer<typeof formSchema>>();
+  const formOfUpload = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema)
+  });
   const [errorMessageName, setErrorMessageName] = useState('');
-  const [errorMessageCollection , setErrorMessageCollection] = useState('');
-  const[errorMessageNftFile , setErrorMessageNftFile] = useState('');
-  const [preview , setPreview] = useState<string>('/icons/default-nft-preview.png');
-  const [collectionErrorMessage , setCollectionErrorMessage] = useState('');
-  const [royaltiesErrorMessage , setRoyaltiesnErrorMessage] = useState('');
-  const [priceErrorMessage , setPriceErrorMessage] = useState('');
-  const [showAlertDialog , setShowAlertDialog] = useState('');
-  const [collection , setColleciton] = useState('');
-  
+  const [errorMessageCollection, setErrorMessageCollection] = useState('');
+  const [errorMessageNftFile, setErrorMessageNftFile] = useState('');
+  const [preview, setPreview] = useState<string>('/icons/default-nft-preview.png');
+  const [collectionErrorMessage, setCollectionErrorMessage] = useState('');
+  const [royaltiesErrorMessage, setRoyaltiesnErrorMessage] = useState('');
+  const [priceErrorMessage, setPriceErrorMessage] = useState('');
+  const [showAlertDialog, setShowAlertDialog] = useState('');
+  const [collection, setColleciton] = useState('');
 
-  const handleOnChange =  (event: React.ChangeEvent<HTMLInputElement>)=>{
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const filePre = new FileReader;
     const file = event.target.files?.[0];
 
     if (file) {
       const fileReader = new FileReader();
-  
+
       fileReader.onload = () => {
         if (typeof fileReader.result === 'string') {  //conditional for string
-          setPreview(fileReader.result); 
+          setPreview(fileReader.result);
         }
       };
-  
+
       fileReader.readAsDataURL(file); // Read the file as a data URL
     }
-    
+
     // filePre.readAsDataURL(tar)
 
   }
-  
+
   // const handleDrop = (acceptedFiles: File[]) => {
   //   // Handle the dropped files here
   //   console.log(acceptedFiles);
@@ -77,8 +79,8 @@ const UploadNFt = () => {
   // });
 
 
-  
-  const handleOnSubmit = (e: React.SyntheticEvent)=>{
+
+  const handleOnSubmit = (e: React.SyntheticEvent) => {
     // console.log(e?.target?.name?.value);
     e.preventDefault();
     const form = e.currentTarget as HTMLFormElement;
@@ -89,71 +91,71 @@ const UploadNFt = () => {
     console.log('in here ');
     console.log(formData.get('collection'))
     console.log(formData.get('royalties'))
-    if(!file){
+    if (!file) {
       setErrorMessageNftFile('Please Upload A file to Proceed');
-    
-    }else{
+
+    } else {
       setErrorMessageNftFile('');
     }
-    if(!formData.get('name')){
+    if (!formData.get('name')) {
       setErrorMessageName('Name must be filed');
-      
-    }else{
+
+    } else {
       setErrorMessageName('')
     }
-    if(!formData.get('collection')){
-        setCollectionErrorMessage('Please fill in the collection name');
-    }else{
+    if (!formData.get('collection')) {
+      setCollectionErrorMessage('Please fill in the collection name');
+    } else {
       setCollectionErrorMessage('');
-    }if(!formData.get('price')){
+    } if (!formData.get('price')) {
       setPriceErrorMessage('Please Enter Price');
-    }else{
+    } else {
       setPriceErrorMessage('');
-    }if(!formData.get('royalties')){
+    } if (!formData.get('royalties')) {
       setRoyaltiesnErrorMessage('Please Enter Royality');
-    }else{
+    } else {
       setRoyaltiesnErrorMessage('');
     }
 
-    if(!errorMessageName || !errorMessageCollection || !errorMessageNftFile || !collectionErrorMessage || !priceErrorMessage){
+    if (!errorMessageName || !errorMessageCollection || !errorMessageNftFile || !collectionErrorMessage || !priceErrorMessage) {
       return;
     }
 
     console.log(file);
-    
 
 
-    
+
+
   }
   return (
     <>
-    <div className='navbar-space'>
-    </div>
-    <HeroNft />
+      <div className='navbar-space'>
+      </div>
+      <HeroNft />
 
 
-    <section className='bg-success-503 '>
-    <div className='mb-8'>
-    <form onSubmit={handleOnSubmit}>
-      <div className='flex flex-col'>
-      <div className='flex justify-between  max-sm:flex-col'>
-        <div className='sm:w-1/2'>
-      <FormRow className='p-4 md:px-8 w-fit h-fit'>
-        <p className='text-white font-montserrat font-semibold text-[22px] mb-2'>Upload Here*</p>
-      <input type='file' name='nftFile' className='hidden' id = 'nftFile' accept=".jpg,.png,.svg,.mp4,.gif" onChange={handleOnChange}/>
-   
-      <label htmlFor="nftFile" className='w-fit h-fit'>
-        <div className='bg-success-520 bg-opacity-55 border-2 border-dotted border-success-506 justify-center items-center cursor-pointer'>
-          <div className='lg:p-24 sm:p-16 flex flex-col items-center justify-center max-sm:p-14'>
-          <Image src = '/icons/Upload-file-icon.png' height={32} width={32}  alt = 'upload'/>
-          <p className='text-white text-[12px] font-montserrat font-semibold '>Max Size : 50MB</p>
-          <p className='text-white text-[12px] font-montserrat font-semibold'>JPG,&nbsp;SVG,&nbsp;PNG,&nbsp;GIF,&nbsp;SV4</p>
-          </div>
-        </div>
-        </label>
+      <section className='bg-success-503 '>
+        <div className='mb-8'>
+          <form onSubmit={handleOnSubmit}>
+            <div className='flex flex-col'>
+              <div className='flex justify-between  max-sm:flex-col'>
+                <div className='sm:w-1/2'>
+                  <FormRow className='p-4 md:px-8 w-fit h-fit'>
+                    <p className='text-white font-montserrat font-semibold text-[22px] mb-2'>Upload Here*</p>
+                    <input type='file' name='nftFile' className='hidden' id='nftFile' accept=".jpg,.png,.svg,.mp4,.gif" onChange={handleOnChange} />
+
+                    <label htmlFor="nftFile" className='w-fit h-fit'>
+                      <div className='bg-success-520 bg-opacity-55 border-2 border-dotted border-success-506 justify-center items-center cursor-pointer'>
+                        <div className='lg:p-24 sm:p-16 flex flex-col items-center justify-center max-sm:p-14'>
+                          <Image src='/icons/Upload-file-icon.png' height={32} width={32} alt='upload' />
+                          <p className='text-white text-[12px] font-montserrat font-semibold '>Max Size : 50MB</p>
+                          <p className='text-white text-[12px] font-montserrat font-semibold'>JPG,&nbsp;SVG,&nbsp;PNG,&nbsp;GIF,&nbsp;SV4</p>
+                        </div>
+                      </div>
+                    </label>
 
 
-{/* <div className='w-fit h-fit'>
+                    {/* <div className='w-fit h-fit'>
       <div {...getRootProps()} className='bg-success-520 bg-opacity-55 border-2 border-dotted border-success-506 justify-center items-center cursor-pointer'>
         <input {...getInputProps()} />
         <div className='p-24 flex flex-col items-center justify-center max-sm:p-10'>
@@ -163,60 +165,60 @@ const UploadNFt = () => {
         </div>
       </div>
     </div> */}
-      {errorMessageNftFile && <p className='text-success-517 text-[11px] font-normal'>{errorMessageNftFile}*</p>}
+                    {errorMessageNftFile && <p className='text-success-517 text-[11px] font-normal'>{errorMessageNftFile}*</p>}
 
-      <p className='text-[12px] text-white text-opacity-65 mt-4 font-thin'>Note: Once your NTF is Minted You can&apos;t be changed any details </p>
-      </FormRow>
-      </div>
-      <div className='sm:w-1/2   flex flex-col p-2 px-8'>
-      <p className='text-white font-montserrat text-[22px] font-semibold'>Preview here</p>
-      <p className='text-white font-montserrat text-[22px] font-thin text-opacity-66'>Uploaded pic will be shown here</p>
-      <div className=' p-2 max-w-[570px] max-h-[262px] bg-success-503 secondary-shadow11 rounded-xl flex items-center justify-center'>
-      <Image src = {preview} height={229} width={521} alt='image' className='max-w-full max-h-full overflow-hidden' />
-      </div>
-      </div>
-      </div>
+                    <p className='text-[12px] text-white text-opacity-65 mt-4 font-thin'>Note: Once your NTF is Minted You can&apos;t be changed any details </p>
+                  </FormRow>
+                </div>
+                <div className='sm:w-1/2   flex flex-col p-2 px-8'>
+                  <p className='text-white font-montserrat text-[22px] font-semibold'>Preview here</p>
+                  <p className='text-white font-montserrat text-[22px] font-thin text-opacity-66'>Uploaded pic will be shown here</p>
+                  <div className=' p-2 max-w-[570px] max-h-[262px] bg-success-503 secondary-shadow11 rounded-xl flex items-center justify-center'>
+                    <Image src={preview} height={229} width={521} alt='image' className='max-w-full max-h-full overflow-hidden' />
+                  </div>
+                </div>
+              </div>
 
-      <div className='flex justify-between  max-sm:flex-col sm:mb-0 gap-2  '>
-      <FormRow className='sm:w-1/2 p-4 md:px-8'>
-        <FormLabel htmlFor='name' className='font-montserrat text-white text-[22px] font-semibold'>Name Of Your NFT*</FormLabel>
-        <InputText id = 'name' name = 'name' type='text' placeHolder='eg-cratoNFT' className='p-3' />
-        {errorMessageName && <p className='text-success-517 text-[11px] font-normal'>{errorMessageName}*</p>}
-      </FormRow>
-      <FormRow className='sm:w-1/2 p-4 md:px-8'>
-        <FormLabel htmlFor='price' className='font-montserrat text-white text-[22px] font-semibold'>Price*</FormLabel>
-        <InputText id = 'price' name = 'price' type='text' placeHolder='1-BITSI' className='p-3' />
-        {priceErrorMessage && <p className='text-success-517 text-[11px] font-normal'>{priceErrorMessage}*</p>}
-      </FormRow>
-      </div>
-      <div className='flex justify-between max-sm:flex-col sm:mb-10 gap-2 '>
-      <FormRow className='sm:w-1/2 p-4 md:px-8'>
-        <FormLabel htmlFor='collection' className='font-montserrat text-white text-[22px] font-semibold'>Collection*</FormLabel>
-        {/* <InputText id = 'collection' name = 'collection' type='text' placeHolder='Please enter the name of your collection' className='p-3' /> */}
-        <input id='collection' name='collection' type='text' placeholder='' className='hidden' value={collection}  />
-        <CollectionCombobox setCollectionValue={setColleciton} />
-        {collectionErrorMessage && <p className='text-success-517 text-[11px] font-normal'>{collectionErrorMessage}*</p>}
-      </FormRow>
-      <FormRow className='sm:w-1/2 p-4 md:px-8'>
-        <FormLabel htmlFor='royalties' className='font-montserrat text-white text-[22px] font-semibold'>Royalties*</FormLabel>
-        <InputText id = 'royalties' name = 'royalties' type='text' placeHolder='Suggested 0. 10%, 20%, 30%, 40% MAX is 70%' className='p-3' />
-        {royaltiesErrorMessage && <p className='text-success-517 text-[11px] font-normal '>{royaltiesErrorMessage}*</p>}
-      </FormRow>
-      </div>
-      <div className='flex max-sm:flex-col sm:items-center sm:mb-10 gap-2  '>
-      <FormRow className='sm:w-1/2 p-4 md:px-8'>
-        <FormLabel htmlFor='name' className='font-montserrat text-white text-[22px] font-semibold'>Description</FormLabel>
-        <InputText id = 'description' name = 'description' type='text' placeHolder='Describe about your NFT' className='p-6' />
-      </FormRow>
-      <div className='h-fit sm:w-1/2 place-content-center items-center max-sm:p-4'>
-      <button className='text-white max-sm:w-full  bg-success-513 sm:px-24  sm:ml-10 lg:px-48  py-4 sm:mt-6 rounded-xl text-[20px]'>MINT
-      </button>
-      </div>
-      </div>
-      </div>
-    </form>
-    </div>
-    </section>
+              <div className='flex justify-between  max-sm:flex-col sm:mb-0 gap-2  '>
+                <FormRow className='sm:w-1/2 p-4 md:px-8'>
+                  <FormLabel htmlFor='name' className='font-montserrat text-white text-[22px] font-semibold'>Name Of Your NFT*</FormLabel>
+                  <InputText id='name' name='name' type='text' placeHolder='eg-cratoNFT' className='p-3' />
+                  {errorMessageName && <p className='text-success-517 text-[11px] font-normal'>{errorMessageName}*</p>}
+                </FormRow>
+                <FormRow className='sm:w-1/2 p-4 md:px-8'>
+                  <FormLabel htmlFor='price' className='font-montserrat text-white text-[22px] font-semibold'>Price*</FormLabel>
+                  <InputText id='price' name='price' type='text' placeHolder='1-BITSI' className='p-3' />
+                  {priceErrorMessage && <p className='text-success-517 text-[11px] font-normal'>{priceErrorMessage}*</p>}
+                </FormRow>
+              </div>
+              <div className='flex justify-between max-sm:flex-col sm:mb-10 gap-2 '>
+                <FormRow className='sm:w-1/2 p-4 md:px-8'>
+                  <FormLabel htmlFor='collection' className='font-montserrat text-white text-[22px] font-semibold'>Collection*</FormLabel>
+                  {/* <InputText id = 'collection' name = 'collection' type='text' placeHolder='Please enter the name of your collection' className='p-3' /> */}
+                  <input id='collection' name='collection' type='text' placeholder='' className='hidden' value={collection} />
+                  <CollectionCombobox setCollectionValue={setColleciton} />
+                  {collectionErrorMessage && <p className='text-success-517 text-[11px] font-normal'>{collectionErrorMessage}*</p>}
+                </FormRow>
+                <FormRow className='sm:w-1/2 p-4 md:px-8'>
+                  <FormLabel htmlFor='royalties' className='font-montserrat text-white text-[22px] font-semibold'>Royalties*</FormLabel>
+                  <InputText id='royalties' name='royalties' type='text' placeHolder='Suggested 0. 10%, 20%, 30%, 40% MAX is 70%' className='p-3' />
+                  {royaltiesErrorMessage && <p className='text-success-517 text-[11px] font-normal '>{royaltiesErrorMessage}*</p>}
+                </FormRow>
+              </div>
+              <div className='flex max-sm:flex-col sm:items-center sm:mb-10 gap-2  '>
+                <FormRow className='sm:w-1/2 p-4 md:px-8'>
+                  <FormLabel htmlFor='name' className='font-montserrat text-white text-[22px] font-semibold'>Description</FormLabel>
+                  <InputText id='description' name='description' type='text' placeHolder='Describe about your NFT' className='p-6' />
+                </FormRow>
+                <div className='h-fit sm:w-1/2 place-content-center items-center max-sm:p-4'>
+                  <button className='text-white max-sm:w-full  bg-success-513 sm:px-24  sm:ml-10 lg:px-48  py-4 sm:mt-6 rounded-xl text-[20px]'>MINT
+                  </button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
     </>
   )
 }
