6409f7a3467965faf9ed49448129498d72fd733e
kalanishivam <kalanishivam@gmail.com>
2024-09-12 18:29:38 +0530

    cache remove2

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -52,6 +52,7 @@ export const metadata: Metadata = {
 };
 export const dynamic = 'force-dynamic';  // this and next line is added to have no cache
 export const revalidate = 0;
+export const fetchCache = 'force-no-store';
 
 export default function RootLayout({
   children,
