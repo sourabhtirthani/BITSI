03c04719b3e2efd33894c4c8a310745195253ab8
kalanishivam <kalanishivam@gmail.com>
2024-09-12 18:39:17 +0530

    cahce no store

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,14 +3,15 @@ const nextConfig = {
     images: {
         domains: ['res.cloudinary.com'],
     },
+    generateEtags: false,
     webpack: config => {
         config.externals.push('pino-pretty', 'lokijs', 'encoding')
         return config
       }
 };
 
-module.exports = {
-    generateEtags: false,
-  }
+// module.exports = {
+//     generateEtags: false,
+//   }
 
 export default nextConfig;
