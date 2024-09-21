079849c828811c8a44b31bb69a46b44dc5da4c60
kalanishivam <kalanishivam@gmail.com>
2024-09-22 02:47:43 +0530

    address updated

--- scriptListner/controllers/eventController.js
+++ scriptListner/controllers/eventController.js
@@ -7,18 +7,26 @@ export const buyEvent = async(from , to , time , price, nftId)=>{
         console.log('in the function of buy event ')
         const nftEvent = 'buy';
         const nftEventForSold = 'Sold';
+
+        const nftPriceQuery = `SELECT nft_price FROM "Nft" WHERE id = ${nftId};`;
+        const nftPriceResult = await client.query(nftPriceQuery)
+        if (nftPriceResult.rows.length === 0) {
+            throw new Error('NFT not found');
+        }
+        const lastPrice = nftPriceResult.rows[0].nft_price;
+        const lossAmount = price - lastPrice;
+
     const insertQuery = `
     INSERT INTO "Nft_events" (nft_event, nft_price, "from", "to", time, "nftId" , asset_name)
     VALUES ('${nftEvent}', ${price}, '${from}', '${to}', '${time.toISOString()}', ${nftId} , 'NFT');`;
 
 
-    const insertQuerySold = `INSERT INTO "Nft_events" (nft_event, nft_price, "from", "to", time, "nftId" , asset_name)
-    VALUES ('${nftEventForSold}', ${price}, '${to}', '${from}', '${time.toISOString()}', ${nftId} , 'NFT');`;
+    const insertQuerySold = `INSERT INTO "Nft_events" (nft_event, nft_price, "from", "to", time, "nftId" , asset_name , loss_amount)
+    VALUES ('${nftEventForSold}', ${price}, '${to}', '${from}', '${time.toISOString()}', ${nftId} , 'NFT' , ${lossAmount});`;
 
-
-     await client.query(insertQuery);
+     await client.query(insertQuery);   
      await client.query(insertQuerySold);
-    const updateQuery = `UPDATE "Nft" SET nft_owner_address = '${to}', up_for_sale = false , nft_price = ${price} WHERE id = ${nftId};`;
+     const updateQuery = `UPDATE "Nft" SET nft_owner_address = '${to}', up_for_sale = false , nft_price = ${price} WHERE id = ${nftId};`;
      await client.query(updateQuery);
 
       const updateInsuraceQuery = `UPDATE "Insurance"  SET "soldValue" = ${price} , "currentOwner" = ${to} WHERE "nftId" = ${nftId};`
